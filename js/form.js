e1810685debd402777fb5c94250ce77ce135de89
MaxBabenko <maxbabnko@gmail.com>
2016-02-08 00:13:01 +0200

    module3-task2

--- js/form.js
+++ js/form.js
@@ -26,9 +26,41 @@
   var reviewHint = document.querySelector('#hint-required-review-field');
   var submitButton = formReviewElement.querySelector('button');
 
+  name.value = docCookies.getItem('name');
+  formReviewElement['review-mark-' + docCookies.getItem('mark')].checked = true;
+
+  checkMarksValue();
+  checkNameValue();
+
   submitButton.disabled = !formIsValid();
 
   marks.onclick = function() {
+    checkMarksValue();
+  };
+
+  name.onchange = function() {
+    checkNameValue();
+  };
+
+  review.onchange = function() {
+    checkReviewValue();
+  };
+
+  formReviewElement.onsubmit = function(evt) {
+    evt.preventDefault();
+
+    var mark = getMark();
+    var expiryDate = 150 * 24 * 60 * 60 * 1000;
+    var dateToExpire = Date.now() + expiryDate;
+    var formattedDateToExpire = new Date(dateToExpire).toUTCString();
+
+    document.cookie = 'name=' + name.value + ';expires=' + formattedDateToExpire;
+    document.cookie = 'mark=' + mark + ';expires=' + formattedDateToExpire;
+
+    formReviewElement.submit();
+  };
+
+  function checkMarksValue() {
     if (mark1.checked || mark2.checked) {
       review.setAttribute('required', true);
       if (review.value === null || review.value === '') {
@@ -40,19 +72,23 @@
       review.removeAttribute('required', true);
       reviewHint.classList.add('invisible');
     }
-    hintsBlockVisible();
-    submitButton.disabled = !formIsValid();
-  };
 
-  name.onchange = function() {
-    if (name.value !== null || name.value !== '') {
-      nameHint.classList.toggle('invisible');
+    setHintsBlockVisible();
+    submitButton.disabled = !formIsValid();
+  }
+
+  function checkNameValue() {
+    if (name.value === null || name.value === '') {
+      nameHint.classList.remove('invisible');
+    } else {
+      nameHint.classList.add('invisible');
     }
-    hintsBlockVisible();
-    submitButton.disabled = !formIsValid();
-  };
 
-  review.onchange = function() {
+    setHintsBlockVisible();
+    submitButton.disabled = !formIsValid();
+  }
+
+  function checkReviewValue() {
     if (mark1.checked || mark2.checked) {
       if (review.value === null || review.value === '') {
         reviewHint.classList.remove('invisible');
@@ -60,9 +96,10 @@
         reviewHint.classList.add('invisible');
       }
     }
-    hintsBlockVisible();
+
+    setHintsBlockVisible();
     submitButton.disabled = !formIsValid();
-  };
+  }
 
   function formIsValid() {
     var isValid = true;
@@ -77,11 +114,19 @@
     return isValid;
   }
 
-  function hintsBlockVisible() {
+  function setHintsBlockVisible() {
     if (nameHint.classList.contains('invisible') && reviewHint.classList.contains('invisible')) {
       hints.classList.add('invisible');
     } else {
       hints.classList.remove('invisible');
+    }
+  }
+
+  function getMark() {
+    for (var i = 1; i < 6; i++) {
+      if(formReviewElement['review-mark-' + i].checked) {
+        return i;
+      }
     }
   }
 })();
